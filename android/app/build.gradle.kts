074025eefc7907e363455b614df27c00242b41ae
akumbom <akumbom5ma@gmail.com>
2025-11-20 07:39:58 +0100

    added Mother of Hope to hymn 242

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 9
-        versionName = "1.0.9"
+        versionCode = 10
+        versionName = "1.1.0"
     }
     
 
