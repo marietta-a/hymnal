2b293e2bb7b061f038cb43dfdacd31b9cc24f982
akumbom <akumbom5ma@gmail.com>
2026-02-19 07:51:25 +0100

    Corrected the lyrics of hymn 300

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 14
-        versionName = "1.1.4"
+        versionCode = 16
+        versionName = "1.1.6"
     }
     
 
