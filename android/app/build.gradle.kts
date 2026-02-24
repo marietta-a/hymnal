92332b756bb43ad7197f68b3c9498b688eccb8d4
akumbom <akumbom5ma@gmail.com>
2026-02-24 12:43:50 +0100

    Corrected the lyrics of Hymn 164

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 20
-        versionName = "1.2.0"
+        versionCode = 22
+        versionName = "1.2.2"
     }
     
 
