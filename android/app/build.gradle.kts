99a6b3f53975cfa0f34a90a70040b3bf43529586
akumbom <akumbom5ma@gmail.com>
2026-05-07 12:14:22 +0100

    Fix daily hymn notification issue

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 34
-        versionName = "0.3.4"
+        versionCode = 35
+        versionName = "0.3.5"
     }
     
 
